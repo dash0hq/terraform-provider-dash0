ce5f84bbb8bc84047c78f20016ee45aa89489d1b
Ben Blackmore <ben.blackmore@dash0.com>
2025-09-08 17:24:03 +0200

    docs: update to reflect latest version

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    dash0 = {
+      source  = "dash0hq/dash0"
+      version = "~> 1.4.0"
+    }
+  }
+}
+
 provider "dash0" {
   # Configuration can be provided via environment variables:
   # DASH0_URL and DASH0_AUTH_TOKEN
