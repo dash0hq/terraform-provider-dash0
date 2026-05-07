d024256698918e0fb90c4468ac7e14bb35f1efc7
Ganesh Tiwari <ganesht049@gmail.com>
2026-05-07 15:24:57 +0530

    feat: added test that make provider unable to find a profile name & 1 with profiles.json have incorrect schema
    Authored-by: ganesht049@gmail.com

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -222,7 +222,7 @@ func (p *dash0Provider) Configure(ctx context.Context, req provider.ConfigureReq
 				}
 			} else {
 				// if `profile` value loaded from `activeProfile` is still empty
-				exceptionHandled = errors.New("Unable to resolve `profile` name from dash0 CLI Files")
+				exceptionHandled = errors.New("Unable to resolve `profile` name from dash0 CLI `activeProfile` as well")
 			}
 		}
 	}
